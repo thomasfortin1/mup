23245b80a9afdb57e7d909f852f00abeadb882aa
thomasfortin1 <thomasfortin@lilve.com>
2024-05-05 22:15:44 -0400

    removed mup.Adam, mup.AdamW, and mup.SGD from package

--- mup/__init__.py
+++ mup/__init__.py
@@ -4,4 +4,4 @@ from mup.shape import *
 from mup.infshape import *
 from mup.init import *
 from mup.layer import *
-from mup.optim import *
+from mup.optim import MuSGD, MuAdam, MuAdamW, process_param_groups
